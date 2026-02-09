45fe4fe8ee0c483d1ada5c976585e216b11301a6
Mikhail Davydenko <mimih1990@gmail.com>
2026-02-09 17:17:07 +0300

    add database parameter

--- src/lib/clickhouse/clickhouse.ts
+++ src/lib/clickhouse/clickhouse.ts
@@ -4,10 +4,12 @@ const host = process.env.CLICKHOUSE_HOST || 'localhost';
 const port = process.env.CLICKHOUSE_PORT || '8123';
 const user = process.env.CLICKHOUSE_USER || 'default';
 const password = process.env.CLICKHOUSE_PASSWORD || 'password';
+const database = process.env.CLICKHOUSE_DATABASE || 'default';
 
 export const clickhouse = createClient({
   url: `http://${host}:${port}`,
   username: user,
   password: password,
+  database: database,
   // You can add more options as needed, e.g. database, compression, etc.
 });
